3a72fae697936cd9d2cdda73a5f2c619883b761b
Niyiragira <niyiragiragerard@gmail.com>
2021-11-22 15:33:35 +0200

    initial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../assets/styles/map.css"
+
+export default function SearchBox() {
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    )
+}
+
